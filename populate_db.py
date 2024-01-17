fff778602715cd7e47d40c8d67fc4e7da4028fdd
Thomas Crowe <wtcrowe4@outlook.com>
2024-01-17 11:47:18 -0500

    alpaca paper trading api working

--- populate_db.py
+++ populate_db.py
@@ -1,9 +1,19 @@
+import os
+from dotenv import load_dotenv
 import sqlite3
+import alpaca_trade_api as tradeapi
+
+load_dotenv()
+alpaca_api_key = os.getenv('ALPACA_API_KEY')
+alpaca_api_secret = os.getenv('ALPACA_API_SECRET')
 
 conn = sqlite3.connect('database.db')
 
 cursor = conn.cursor()
 
-cursor.execute("DELETE FROM stock")
+api = tradeapi.REST(alpaca_api_key, alpaca_api_secret, base_url='https://paper-api.alpaca.markets')
+assets = api.list_assets()
+for asset in assets:
+    print(asset)
 
 conn.commit()
