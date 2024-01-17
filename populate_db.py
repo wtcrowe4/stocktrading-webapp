43140fdf63291bdb3b74c5be7a20b0d7d37a10c6
Thomas Crowe <wtcrowe4@outlook.com>
2024-01-17 12:46:05 -0500

    populated db with only tradable and active stocks

--- populate_db.py
+++ populate_db.py
@@ -13,7 +13,14 @@ cursor = conn.cursor()
 
 api = tradeapi.REST(alpaca_api_key, alpaca_api_secret, base_url='https://paper-api.alpaca.markets')
 assets = api.list_assets()
+
 for asset in assets:
-    print(asset)
+    #print(asset.name)
+    try:
+        if asset.status == 'active' and asset.tradable:
+            cursor.execute('INSERT INTO stock (symbol, company) VALUES (?, ?)', (asset.symbol, asset.name))
+    except Exception as e:
+        print(asset.symbol)
+        print(e)
 
 conn.commit()
