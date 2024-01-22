b018a30dfd1f32d8aeadf7df2591b8adb2c19dfa
Thomas Crowe <wtcrowe4@outlook.com>
2024-01-22 13:35:48 -0500

    home page connected to stock table

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 from fastapi import FastAPI
+import sqlite3
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
+db_url = os.getenv('DATABASE_URL')
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Home page"}
+    conn = sqlite3.connect(db_url)
+    cursor = conn.cursor()
+    conn.row_factory = sqlite3.Row
+    cursor.execute("SELECT symbol FROM stock")
+    rows = cursor.fetchall()
+    return {"message": rows}
+    
+    #return {"message": "Home page"}
 
 @app.get("/stocks")
 async def stocks():
-    return {"message": "Specific stock"}
+    return {"message": "Specific stocks"}
 
 
 # @app.get("/stocks/{symbol}")
