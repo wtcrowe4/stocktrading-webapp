7b0c4e93ede88223522b11e18db0ef3f1d850727
Thomas Crowe <wtcrowe4@outlook.com>
2024-02-13 06:41:39 -0500

    added exchange to stock schema for widget

--- populate_db.py
+++ populate_db.py
@@ -32,7 +32,7 @@ for asset in assets:
     #print(asset.name)
     try:
         if asset.status == 'active' and asset.tradable and asset.symbol not in symbols:
-            cursor.execute('INSERT INTO stock (symbol, name) VALUES (?, ?)', (asset.symbol, asset.name))
+            cursor.execute('INSERT INTO stock (symbol, name, exchange) VALUES (?, ?, ?)', (asset.symbol, asset.name, asset.exchange))
             print(f'Added a new stock {asset.symbol} {asset.name} to the database')
     except Exception as e:
         print(asset.symbol)
