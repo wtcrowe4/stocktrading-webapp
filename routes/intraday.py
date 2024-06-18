f67881e25eafc0bcf90eaf021b845a43517f8d14
Thomas Crowe <wtcrowe4@outlook.com>
2024-06-18 14:03:31 -0400

    cleaning up unused code

--- routes/intraday.py
+++ routes/intraday.py
@@ -68,38 +68,3 @@ async def intraday_lows(request: Request):
 app.include_router(router, tags=['intraday'])
 
 
-
-
-# #Page for Intraday Highs
-# @app.get("/intraday_highs")
-# async def intraday_highs(request: Request):
-#     conn = sqlite3.connect(db_url)
-#     conn.row_factory = sqlite3.Row
-#     cursor = conn.cursor()
-#     cursor.execute("""
-#         SELECT symbol, name, date, high
-#         FROM stock JOIN stock_price ON stock.id = stock_price.stock_id
-#         WHERE date = (SELECT MAX(date) FROM stock_price)
-#         ORDER BY high DESC
-#         LIMIT 50
-#     """)
-#     rows = cursor.fetchall()
-#     return templates.TemplateResponse("intraday.html", {"request": request, "stocks": rows})
-
-
-# #Page for Intraday Lows
-# @app.get("/intraday_lows")
-# async def intraday_lows(request: Request):
-#     conn = sqlite3.connect(db_url)
-#     conn.row_factory = sqlite3.Row
-#     cursor = conn.cursor()
-#     cursor.execute("""
-#         SELECT symbol, name, date, low
-#         FROM stock JOIN stock_price ON stock.id = stock_price.stock_id
-#         WHERE date = (SELECT MAX(date) FROM stock_price)
-#         ORDER BY low ASC
-#         LIMIT 50
-#     """)
-#     rows = cursor.fetchall()
-    
-#     return templates.TemplateResponse("intraday.html", {"request": request, "stocks": rows})
